773181b258d23903d157b4bd3c6ce3d3bf81b7f7
Paul Anthony McGowan <xhostcom@aol.com>
2022-12-16 11:29:12 +0000

    Initial commit

---
+++ spec/factories/announcements.rb
@@ -0,0 +1,20 @@
+# == Schema Information
+#
+# Table name: announcements
+#
+#  id                :bigint           not null, primary key
+#  announcement_type :string
+#  description       :text
+#  name              :string
+#  published_at      :datetime
+#  created_at        :datetime         not null
+#  updated_at        :datetime         not null
+#
+FactoryBot.define do
+  factory :announcement do
+    published_at { "2022-12-16 11:20:37" }
+    announcement_type { "MyString" }
+    name { "MyString" }
+    description { "MyText" }
+  end
+end
